af3bdf4f24b06594bb52f6e7f36de5814dbb0fc6
Nishith Nishith <lnu.nish@northeastern.edu>
2024-06-02 15:04:28 -0400

    prototype comments

--- prototype.py
+++ prototype.py
@@ -1,5 +1,9 @@
 import copy
 
+#python has inbuild methods for copy and deep copy. For other languages, look into the copy and deep copy implementation
+# copy - gives a clone of the structure in a high level, the nested objects are not cloned
+# deep copy - the object and the nested objects are cloned.
+
 class Prototype:
     def __init__(self, name) -> None:
         self.name = name
@@ -12,7 +16,7 @@ class Prototype:
 
 
 if(__name__ == '__main__'):
-    p1 : Prototype = Prototype('first_proto')
+    p1:Prototype = Prototype('first_proto')
     p1.display()
 
     p2:Prototype = p1.clone()
