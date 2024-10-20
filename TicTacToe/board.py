c26b122cbdd13e443410c724b9af7ff661f0b81b
Nishith <Nishith0514@gmail.com>
2024-10-20 16:15:30 -0400

    completed game logic

--- TicTacToe/board.py
+++ TicTacToe/board.py
@@ -16,8 +16,24 @@ class Board:
             self.board_count +=1
             self.print_board()
 
-    def check_win(self):
-        pass
+    def check_win(self, player: Player):
+        sym = player.get_symbol()
+
+        for i in range(3):
+            if(self.board[i][0]!='-' and (self.board[i][0]==self.board[i][1]==self.board[i][2]==sym)):
+                return True
+        
+        for i in range(3):
+            if(self.board[0][i]!='-' and (self.board[0][i]==self.board[1][i]==self.board[2][i]==sym)):
+                return True
+        
+        if(self.board[0][0]==self.board[1][1]==self.board[2][2]==sym):
+            return True
+        
+        if(self.board[0][2]==self.board[1][1]==self.board[2][0]==sym):
+            return True
+        
+        return False
 
     def is_finish(self):
         if(self.board_count==9):
--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -6,6 +6,26 @@ class Game:
         self.player1 = player1
         self.player2 = player2
         self.board = Board()
+        self.current_player = self.player1
     
     def play(self):
-        pass
+        self.board.print_board()
+        while(self.board.is_finish()==False):
+            print('turn',self.current_player.get_name())
+            r = int(input('enter row '))
+            c = int(input('enter column '))
+
+            self.board.make_move(self.current_player,r,c)
+
+            if(self.board.check_win(self.current_player)==True):
+                print('winner!',self.current_player.get_name())
+                return
+            self.switch_player()
+
+        print('draw')
+
+    def switch_player(self):
+        if(self.current_player!=self.player1):
+            self.current_player = self.player1
+        else:
+            self.current_player = self.player2
