aae2b097f2c7d1e18e9a888bb7e1f0fe56c715a7
Nishith <Nishith0514@gmail.com>
2024-11-15 17:33:32 -0500

    wildcard matching DP

--- dp/wildcard-matching.py
+++ dp/wildcard-matching.py
@@ -26,3 +26,41 @@ class Solution:
         
         res = util(n-1, m-1)
         return res
+
+
+#DP
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        
+        n = len(s)
+        m = len(p)
+
+        dp = []
+        for i in range(n+1):
+            val = [False] * (m+1)
+            dp.append(val)
+        
+        dp[0][0] = True
+
+        for i in range(1,n+1):
+            dp[i][0] = False
+        
+        for j in range(1, m+1):
+            dp[0][j] = True
+            for k in range(1,j+1):
+                if(p[k-1]!='*'):
+                    dp[0][j] = False
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+            
+                if(s[i-1]==p[j-1] or p[j-1]=='?'):
+                    dp[i][j] = dp[i-1][j-1]
+                
+                elif(p[j-1]=='*'):
+                    dp[i][j] =  dp[i-1][j] or dp[i][j-1]
+                
+                else:
+                    dp[i][j] = False
+        
+        return dp[n][m]
