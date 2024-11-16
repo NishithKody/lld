eaa7703c99127947dad54f269b39a37f049e53d4
Nishith <Nishith0514@gmail.com>
2024-11-15 21:52:28 -0500

    min insertions to make palindrom

--- dp/min-insertions-to-make-pal.py
+++ dp/min-insertions-to-make-pal.py
@@ -1,3 +1,31 @@
+#find the longest palindrome substring, then subtract from the length
+
+class Solution:
+    def minInsertions(self, s: str) -> int:
+
+        def longestPalindromeSubseq(s: str) -> int:
+            n = len(s)
+            r = s[::-1]
+
+            dp = []
+            
+            for i in range(n+1):
+                tmp = [0] * (n+1)
+                dp.append(tmp)
+            
+            for i in range(1,n+1):
+                for j in range(1,n+1):
+                    if(s[i-1] == r[j-1]):
+                        dp[i][j] =  dp[i-1][j-1] + 1
+                    else:
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            
+            return dp[n][n]
+        
+        lps = longestPalindromeSubseq(s)
+        return len(s)-lps
+
+
 class Solution:
     def minInsertions(self, s: str) -> int:
         
