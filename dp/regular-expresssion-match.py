519f1419bff4df441bc6dd0eee82b12324c59246
Nishith <Nishith0514@gmail.com>
2024-11-17 14:15:09 -0500

    regular expression matching

---
+++ dp/regular-expresssion-match.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        n = len(s)
+        m = len(p)
+
+        def util(i,j):
+            if(i>=n and j>=m):
+                return True
+
+            if(j>=m):
+                return False
+            
+            match = i<n and (s[i] == p[j] or p[j] == '.')
+
+            if(j+1<m and p[j+1]=='*'):
+                skip_star = util(i, j+2)
+                sel_star = match and util(i+1, j)
+                return skip_star or sel_star
+            else:
+                return match and util(i+1, j+1)
+
+        res = util(0,0)
+        return res
