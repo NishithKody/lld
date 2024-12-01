944be6c79e9d4a4fc3ab82daeb322da036b57b70
Nishith <Nishith0514@gmail.com>
2024-12-01 18:40:53 -0500

    decode ways dp

--- dp/decode-ways.py
+++ dp/decode-ways.py
@@ -23,3 +23,30 @@ class Solution:
 
         res = util(0)
         return res
+    
+
+    #dp
+    class Solution:
+        def numDecodings(self, s: str) -> int:
+            
+            n = len(s)
+            dp = [0] * (n+1)
+
+            dp[n] = 1
+
+            for i in range(n-1, -1, -1):
+
+                if(s[i] == '0'):
+                    continue
+                
+                single = 0 
+                double = 0
+
+                single = dp[i+1] 
+                
+                if(i+1 < n and 10 <= int(s[i:i+2]) <=26):
+                    double = dp[i+2]
+                            
+                dp[i] =  single + double
+
+            return dp[0]
