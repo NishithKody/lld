a2ce5380350e4fda50bbbdef803117923fc2e4be
Nishith <Nishith0514@gmail.com>
2024-12-09 17:02:30 -0500

    Distinct sub dp

--- dp/distinct-subsequences.py
+++ dp/distinct-subsequences.py
@@ -21,4 +21,32 @@ class Solution:
 
         res = util(0,0)
         return res
+
+#dp
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n = len(s)
+        m = len(t)
+
+        dp = []
+        for i in range(n+1):
+            val = [0] * (m+1)
+            dp.append(val)
+        
+        for i in range(n+1):
+            dp[i][m] = 1
+
+        
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1 , -1):
+        
+                if(s[i] == t[j]):
+                    sel = dp[i+1][j+1]
+                    dsel = dp[i+1][j]
+                    dp[i][j] = sel + dsel
+                else:
+                    dp[i][j] =  dp[i+1][j]
+        
+        return dp[0][0]
     
